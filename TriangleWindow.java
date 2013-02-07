5389d318bc960e4d1cf05262ba6e3f964854b67f
leonardnr <leonardnr@s.dcsdk12.org>
2013-02-07 08:58:56 -0700

    created TriangleWindow class and SirTri class

---
+++ TriangleWindow.java
@@ -0,0 +1,13 @@
+/**
+ * TriangleWindow
+ *
+ * 		A window with a Sierpinski triangle
+ */
+public class TriangleWindow extends JFrame implents MouseActionListener{
+
+	private SirTri = null;
+
+	
+
+
+}
